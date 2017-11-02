030ff8058b64a7e45108ac8c844eb72dc949de51
sherman <herman.cani@gmail.com>
2017-11-02 14:55:28 -0300

    se crea una clase cliente ,se añade un hola mundo

--- Prueba_2/src/prueba_2/Prueba_2.java
+++ Prueba_2/src/prueba_2/Prueba_2.java
@@ -4,6 +4,8 @@ public class Prueba_2 {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        
+        System.out.println("hola mundo2");
     }
-
+    
 }
